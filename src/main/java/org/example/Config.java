d9f183abbca2da5efa4430a7a65e817b618fdd3a
Yana <j13ana9d@yandex.ru>
2024-07-07 21:56:39 +0300

    Second commit

--- src/main/java/org/example/Config.java
+++ src/main/java/org/example/Config.java
@@ -7,36 +7,35 @@ import org.springframework.context.annotation.Configuration;
 public class Config {
     @Bean
     public Parrot parrot1() {
-       Parrot parrot1 = new Parrot();
-       parrot1.setName("Ара");
-       return parrot1;
+        Parrot parrot1 = new Parrot();
+        parrot1.setName("Ара");
+        return parrot1;
     }
 
     @Bean
     public Parrot parrot2() {
         Parrot parrot1 = new Parrot();
         parrot1.setName("Жора");
-        return parrot1;    }
+        return parrot1;
+    }
+
     @Bean
     public Cat cat() {
         Cat cat = new Cat();
         cat.setName("Барсик");
         return cat;
     }
+
     @Bean
     public Dog dog() {
         Dog dog = new Dog();
         dog.setName("Настя");
         return dog;
     }
+
     @Bean
-    public Person person() {
-        Person person = new Person();
-        person.setName("Катя");
-        person.setParrot1(parrot1());
-        person.setParrot2(parrot2());
-        person.setCat(cat());
-        person.setDog(dog());
+    public Person person(Parrot parrot1, Parrot parrot2, Cat cat, Dog dog) {
+        Person person = new Person(parrot1, parrot2, cat, dog);
         return person;
     }
 }
